b1469c43cf1036eae23c2c78d24e9cb1834aade2
Md Shahriyar Al Mustakim Mitul <shahriyarmitul3737@gmail.com>
2021-08-15 11:56:03 +0600

    up

--- Assignment/Assignment 6/10.py
+++ Assignment/Assignment 6/10.py
@@ -1,7 +1,8 @@
-def make_square(val,val_0):
+def make_square(tup):
+    val,val_0=tup
     val_1={}
     for i in range(val,val_0+1):
         val_1[i]=i*i
     print(val_1)
     return " "
-print(make_square(1,3))
+print(make_square((1,3)))
---
+++ Datacamp/main.py
@@ -0,0 +1,8 @@
+def make_square(tup):
+    val,val_0=tup
+    val_1={}
+    for i in range(val,val_0+1):
+        val_1[i]=i*i
+    print(val_1)
+    return " "
+print(make_square((1,3)))
