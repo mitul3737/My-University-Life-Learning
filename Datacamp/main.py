08652ae1dd03e3263c93c13973a5f80bc13fd084
Md Shahriyar Al Mustakim Mitul <shahriyarmitul3737@gmail.com>
2021-09-11 01:23:54 +0600

    dui

--- Datacamp/main.py
+++ Datacamp/main.py
@@ -1,22 +1,19 @@
-def function_name(val_1,val_2):
-    list_1=[]
-    flag=True
-    for j in val_1:
-        for i in range(1, len(val_1)):
-            if abs(j - val_1[i]) == val_2:
-                list_2 = []
-                list_2.append(j)
-                list_2.append(val_1[i])
-                list_1.append(list_2)
-                print(list_1)
-            else:
-                flag = False
-    if flag:
-        print(list_1)
-    else:
-        "Not Possible"
+file_1 = open('E:\My University life learning\My-University-Life-Learning\Datacamp\code_2.txt', 'r')
+file_2 = open('E:\My University life learning\My-University-Life-Learning\Datacamp\code_2_0.txt', 'w')
+string_01 = ""
+string_02 = ""
+new_list_1 = []
+new_list_2 = []
+string_01 = file_1.read()
+new_list = string_01.split(" ")
+for i in range(0, len(new_list)):
+    if new_list[i] != "":
+        new_list_2.append(new_list[i])
 
-    return " "
+for i in new_list_2:
+    string_02 += i + ' '
 
-
-function_name([1, 5, 9, 15, 74], 4)
+print(string_02)
+file_2.write(string_02)
+file_1.close()
+file_2.close()
