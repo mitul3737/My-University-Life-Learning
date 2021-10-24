5b45caae9d773cf415d796afd6b9c76c71b64b8a
Umme Abira Azmary Mouly <86238639+Mouly2000@users.noreply.github.com>
2021-10-24 21:55:19 +0600

    Update 15.py
    thik koro chele

--- CSE111/Assignment 1/15.py
+++ CSE111/Assignment 1/15.py
@@ -8,6 +8,6 @@ for i in srt_0:
         lst_1=[str_1[0]]
         dict_0[str_1[1]]=lst_1
     else:
-       dict_0[str_1[1]].append(str_1[0])
+        dict_0[str_1[1]].append(str_1[0])
     lst_1=[]
 print(dict_0)
